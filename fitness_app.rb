7392db3416f032fc02aeec16304867ebe64e7447
Dimesky <matthew.dimesky@gmail.com>
2016-10-13 11:12:33 -0700

    add comments explaining purpose of app

---
+++ fitness_app.rb
@@ -0,0 +1,15 @@
+# This app will ask the user if they are a man or a woman, how old they are, 
+# how much they weigh, and what their favorite exercise is. It will then allow the 
+# user to enter foods that they eat, either by calories or recognized foods (and mounts of
+# them). The app will then store the user information in a table, and also store the
+# foods that they eat in a separate table (if the food is unrecognized the app will store
+# it to be remembered later).
+#
+# The app will also initially ask the user if they are looking to maintain wieght, 
+# lose weight, or gain weight, and their favorite exercises and foods, andprovide them with
+# instructions for the day for food intake & exercise.
+#
+# Eventually, the app will be able to give users suggestions about foods that they commonly
+# eat that can be cut out of their diet (with reminders based on when they usually eat
+# those foods), and give people reminders throughout the day to exercise based on their
+# total food intake for the day.
